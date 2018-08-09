edc017ddc1597d626ebd189aa133349ef9470561
Винокуров Сергей Сергеевич <Vinokurov_S@Dept07>
2018-08-09 19:35:03 +0300

    PubSub duplication added

--- todolist/src/View.js
+++ todolist/src/View.js
@@ -8,6 +8,7 @@ let onDoneItemCallback = ()=>undefined;
 let onRemoveItemCallback = ()=>undefined;
 
 const Delegate = require("dom-delegate").Delegate;
+const PubSub = require("pubsub-js");
 
 function init (callbacks) {
     onClearListCallback = callbacks.onClearListCallback;
@@ -16,32 +17,52 @@ function init (callbacks) {
     onDoneItemCallback = callbacks.onDoneItemCallback;
     onRemoveItemCallback = callbacks.onRemoveItemCallback;
     
-    let btnRemoveListDelegate = new Delegate(window.document.body);
-    btnRemoveListDelegate.on("click", 'div[data-input-name="remove-list-button"]', onRemoveList);
-    
-    let btnClearListDelegate = new Delegate(window.document.body);
-    btnClearListDelegate.on("click", 'button[data-input-name="clear-list-button"]', onClearList);
-    
-    let btnSubmitDelegate = new Delegate(window.document.body);
-    btnSubmitDelegate.on("click", 'button[data-input-name="submit-button"]', onSubmit);
-    
-    let keyEnterSubmitDelegate = new Delegate(window.document.body);
-    keyEnterSubmitDelegate.on(
+    let delegate = new Delegate(window.document.body);
+    delegate.on(
+        "click", 
+        '.remove-list-button', 
+        function(event, target) {
+            onRemoveList(target.parentNode.parentNode);
+        }
+    );
+    delegate.on(
+        "click", 
+        '.clear-list-button', 
+        function(event, target) {
+            onClearList(target.parentNode.parentNode);
+        }
+    );
+    delegate.on(
+        "click", 
+        '.submit-button', 
+        function(event, target) {
+            submitPublisher(target);
+        }
+    );
+    delegate.on(
         "keypress", 
-        'input[data-input-name="submit-field"]',
+        '.submit-field',
         function (event, target) {
             var key = event.which || event.keyCode;
             if (key === 13) { 
-                onSubmit(event, target); 
+                submitPublisher(target);
             }
         }
     );
-    
-    let btnDoneItemDelegate = new Delegate(window.document.body);
-    btnDoneItemDelegate.on("click", 'button[data-input-name="done-item-button"]', onDoneItem);
-    
-    let btnRemoveItemDelegate = new Delegate(window.document.body);
-    btnRemoveItemDelegate.on("click", 'button[data-input-name="remove-item-button"]', onRemoveItem);
+    delegate.on(
+        "click", 
+        '.done-item-button', 
+        function(event, target) {
+            onDoneItem(target.parentNode);
+        }
+    );
+    delegate.on(
+        "click", 
+        '.remove-item-button', 
+        function(event, target) {
+            onRemoveItem(target.parentNode);
+        }
+    );
 }
 
 function addList(node, listId, list) {
@@ -49,10 +70,7 @@ function addList(node, listId, list) {
     let root = window.document.importNode(tmpl.content, true).querySelector("div");
     root.id = listId;
 
-    let head = root.querySelector("div");
     let todolist = root.querySelector("ol");
-
-    let textfield = head.querySelector("input");
     
     for (let i = 0; i < list.length; ++i) {
         let text = list[i].text;
@@ -63,6 +81,13 @@ function addList(node, listId, list) {
     }
 
     node.appendChild(root);
+
+    let token = PubSub.subscribe(
+        "ON_SUBMIT", 
+        function (msg, text) {
+            onSubmit(root, text);
+        }
+    );
 }
 
 function createItem(itemIndex, text, type) {
@@ -72,19 +97,23 @@ function createItem(itemIndex, text, type) {
     item.setAttribute("itemIndex", itemIndex);
     item.className = "todoitem";
 
-    let p = item.querySelector("span");
-    p.innerHTML = text;
+    let span = item.querySelector("span");
+    span.innerHTML = text;
     if (type == DONE) {
-        p.className = "donefont";
+        span.className = "donefont";
     }
 
     return item;
 }
 
-function onSubmit(event, target) {
+function submitPublisher(target) {
     let root = target.parentNode.parentNode;
     let textfield = root.querySelector("div").querySelector("input");
-    let text = textfield.value;
+    PubSub.publish("ON_SUBMIT", textfield.value);
+    textfield.value = "";
+}
+
+function onSubmit(root, text) {
     if (!text)
         return;
 
@@ -93,43 +122,39 @@ function onSubmit(event, target) {
     let item = createItem(itemIndex, text, TODO);
     todolist.appendChild(item);
 
-    textfield.value = "";
-
     let listId = root.id;
     onSubmitCallback(listId, text);
 }
 
-function onClearList(event, target) {
-    let root = target.parentNode.parentNode;
+function onClearList(root) {
     let todolist = root.querySelector("ol");
     todolist.innerHTML = "";
     onClearListCallback(root.id);
 }
 
-function onRemoveList(event, target) {
-    let root = target.parentNode.parentNode;
+function onRemoveList(root) {
     let parent = root.parentNode;
     parent.removeChild(root);
     onRemoveListCallback(root.id);
 }
 
-function onDoneItem(event, target) {
-    let item = target.parentNode;
-    let p = item.querySelector("span");
-    p.className = "donefont";
+function onDoneItem(item) {
+    let span = item.querySelector("span");
+    span.className = "donefont";
 
     let root = item.parentNode.parentNode;
     onDoneItemCallback(root.id, item.getAttribute("itemIndex"));
 }
 
-function onRemoveItem(event, target) {
-    let item = target.parentNode;
+function onRemoveItem(item) {
     let todolist = item.parentNode;
     let itemIndex = +item.getAttribute("itemIndex");
+
+    let i = itemIndex;
     let next = item.nextSibling;
 
     while (next !== null) {
-        next.setAttribute("itemIndex", itemIndex++);
+        next.setAttribute("itemIndex", i++);
         next = next.nextSibling;
     }
 
